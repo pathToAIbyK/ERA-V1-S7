163f6bea3194e61ef3247e3522b3ab50540d2bab
pathToAIbyK <133480297+pathToAIbyK@users.noreply.github.com>
2023-06-15 15:46:09 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,1 +1,10 @@
+%matplotlib inline
+import matplotlib.pyplot as plt
 
+def return_dataset_images():
+  figure = plt.figure()
+  num_of_images = 60
+  for index in range(1, num_of_images + 1):
+      plt.subplot(6, 10, index)
+      plt.axis('off')
+      plt.imshow(images[index].numpy().squeeze(), cmap='gray_r')
