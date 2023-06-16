8bb7b7f638f5279b54996973bbb82a273bdd5409
pathToAIbyK <133480297+pathToAIbyK@users.noreply.github.com>
2023-06-16 13:50:06 +0530

    Update model.py

--- model.py
+++ model.py
@@ -9,15 +9,15 @@ import matplotlib.pyplot as plt
 class Net_1(nn.Module):
     def __init__(self):
         super(Net_1, self).__init__()
-        self.conv1 = nn.Conv2d(1, 32, 3, padding=1) # 28>28 | 3
-        self.conv2 = nn.Conv2d(32, 64, 3, padding=1) # 28 > 28 |  5
+        self.conv1 = nn.Conv2d(1, 4, 3, padding=1) # 28>28 | 3
+        self.conv2 = nn.Conv2d(4, 4, 3, padding=1) # 28 > 28 |  5
         self.pool1 = nn.MaxPool2d(2, 2) # 28 > 14 | 10
-        self.conv3 = nn.Conv2d(64, 128, 3, padding=1) # 14> 14 | 12
-        self.conv4 = nn.Conv2d(128, 256, 3, padding=1) #14 > 14 | 14
+        self.conv3 = nn.Conv2d(4, 8, 3, padding=1) # 14> 14 | 12
+        self.conv4 = nn.Conv2d(8, 8, 3, padding=1) #14 > 14 | 14
         self.pool2 = nn.MaxPool2d(2, 2) # 14 > 7 | 28
-        self.conv5 = nn.Conv2d(256, 512, 3) # 7 > 5 | 30
-        self.conv6 = nn.Conv2d(512, 1024, 3) # 5 > 3 | 32 | 3*3*1024 | 3x3x1024x10 | 
-        self.conv7 = nn.Conv2d(1024, 10, 3) # 3 > 1 | 34 | > 1x1x10
+        self.conv5 = nn.Conv2d(8, 8, 3) # 7 > 5 | 30
+        self.conv6 = nn.Conv2d(4, 4, 3) # 5 > 3 | 32 | 3*3*1024 | 3x3x1024x10 | 
+        self.conv7 = nn.Conv2d(4, 10, 3) # 3 > 1 | 34 | > 1x1x10
         
     def forward(self, x):
         x = self.pool1(F.relu(self.conv2(F.relu(self.conv1(x)))))
@@ -57,7 +57,31 @@ class Net_2(nn.Module):
     def model_summary(model, input_size):
         summary(model, input_size)
 
+#model 3
+class Net_3(nn.Module):
+    def __init__(self):
+        super(Net_3, self).__init__()
+        self.conv1 = nn.Conv2d(1, 8, 3, padding=1) # 28>28 | 3
+        self.conv2 = nn.Conv2d(8, 16, 3, padding=1) # 28 > 28 |  5
+        self.pool1 = nn.MaxPool2d(2, 2) # 28 > 14 | 10
+        self.conv3 = nn.Conv2d(16, 32, 3, padding=1) # 14> 14 | 12
+        self.conv4 = nn.Conv2d(32, 16, 3, padding=1) #14 > 14 | 14
+        self.pool2 = nn.MaxPool2d(2, 2) # 14 > 7 | 28
+        self.conv5 = nn.Conv2d(16, 16, 3) # 7 > 5 | 30
+        self.conv6 = nn.Conv2d(16, 16, 3) # 5 > 3 | 32 | 3*3*1024 | 3x3x1024x10 | 
+        self.conv7 = nn.Conv2d(16, 10, 3) # 3 > 1 | 34 | > 1x1x10
 
+    def forward(self, x):
+        x = self.pool1(F.relu(self.conv2(F.relu(self.conv1(x)))))
+        x = self.pool2(F.relu(self.conv4(F.relu(self.conv3(x)))))
+        x = F.relu(self.conv6(F.relu(self.conv5(x))))
+        # x = F.relu(self.conv7(x))
+        x = self.conv7(x)
+        x = x.view(-1, 10) #1x1x10> 10
+        return F.log_softmax(x, dim=-1)
+
+    def model_summary(model, input_size):
+        summary(model, input_size)
 
 
 #Train and Test
