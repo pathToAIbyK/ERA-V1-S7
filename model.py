afd4743f2e57daeffb916e9aed482a714f639a39
pathToAIbyK <133480297+pathToAIbyK@users.noreply.github.com>
2023-06-16 15:37:20 +0530

    Update model.py

--- model.py
+++ model.py
@@ -97,7 +97,7 @@ class Net_4(nn.Module):
         self.conv5 = nn.Conv2d(8, 8, 3) # 7 > 5 | 30
         self.conv6 = nn.Conv2d(8, 16, 3) # 5 > 3 | 32 | 3*3*32 | 3x3x32x10 | 
         self.conv7 = nn.Conv2d(16, 10, 3) # 3 > 1 | 34 | > 1x1x10
-        self.dropout = nn.Dropout(0.5)
+        self.dropout = nn.Dropout(0.1)
     def forward(self, x):
         x = self.pool1(self.dropout(F.relu(self.conv2(F.relu(self.conv1(x))))))
         x = self.pool2(self.dropout(F.relu(self.conv4(self.dropout(F.relu(self.conv3(x)))))))
