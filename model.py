dd6b56db6b68d87a4ce085c7509f3fe317aa479f
pathToAIbyK <133480297+pathToAIbyK@users.noreply.github.com>
2023-06-16 13:45:45 +0530

    Update model.py

--- model.py
+++ model.py
@@ -35,15 +35,15 @@ class Net_1(nn.Module):
 class Net_2(nn.Module):
     def __init__(self):
         super(Net_2, self).__init__()
-        self.conv1 = nn.Conv2d(1, 32, 3, padding=1) # 28>28 | 3
-        self.conv2 = nn.Conv2d(32, 64, 3, padding=1) # 28 > 28 |  5
+        self.conv1 = nn.Conv2d(1, 8, 3, padding=1) # 28>28 | 3
+        self.conv2 = nn.Conv2d(8, 16, 3, padding=1) # 28 > 28 |  5
         self.pool1 = nn.MaxPool2d(2, 2) # 28 > 14 | 10
-        self.conv3 = nn.Conv2d(64, 128, 3, padding=1) # 14> 14 | 12
-        self.conv4 = nn.Conv2d(128, 256, 3, padding=1) #14 > 14 | 14
+        self.conv3 = nn.Conv2d(16, 32, 3, padding=1) # 14> 14 | 12
+        self.conv4 = nn.Conv2d(32, 16, 3, padding=1) #14 > 14 | 14
         self.pool2 = nn.MaxPool2d(2, 2) # 14 > 7 | 28
-        self.conv5 = nn.Conv2d(256, 512, 3) # 7 > 5 | 30
-        self.conv6 = nn.Conv2d(512, 1024, 3) # 5 > 3 | 32 | 3*3*1024 | 3x3x1024x10 | 
-        self.conv7 = nn.Conv2d(1024, 10, 3) # 3 > 1 | 34 | > 1x1x10
+        self.conv5 = nn.Conv2d(16, 16, 3) # 7 > 5 | 30
+        self.conv6 = nn.Conv2d(16, 16, 3) # 5 > 3 | 32 | 3*3*1024 | 3x3x1024x10 | 
+        self.conv7 = nn.Conv2d(16, 10, 3) # 3 > 1 | 34 | > 1x1x10
 
     def forward(self, x):
         x = self.pool1(F.relu(self.conv2(F.relu(self.conv1(x)))))
