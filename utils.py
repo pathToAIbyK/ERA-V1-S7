911151c56ee15704a61e94c265025c1149d614c7
pathToAIbyK <133480297+pathToAIbyK@users.noreply.github.com>
2023-06-16 12:41:49 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,9 +1,10 @@
 import matplotlib.pyplot as plt
 
-dataiter = iter(train_loader)
-images, labels = next(dataiter)
+
 
 def return_dataset_images(train_loader,total_images):
+  dataiter = iter(train_loader)
+  images, labels = next(dataiter)
   figure = plt.figure()
   for index in range(1, total_images + 1):
       plt.subplot(6, 10, index)
